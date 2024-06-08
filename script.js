4ba5789c5695f680f780062eabedc0095e592a1b
DanewellXalynx <141164215+DanewellXalynx@users.noreply.github.com>
2024-06-08 12:13:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Print ready
+console.log("ready")
+
+//print some warnings
+console.log("HOLD UP!")
+console.log("If somebody told you to paste something here, you have an 11/10 of being scammed")
